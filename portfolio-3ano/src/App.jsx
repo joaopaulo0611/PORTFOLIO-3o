7ad99f17406cc83d613428f5348e0dcce123267e
JOÃO PAULO ARAUJO CAPPELETTI <joao_cappeletti@estudante.sesisenai.org.br>
2024-02-29 12:17:03 -0300

    salvo

---
+++ portfolio-3ano/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div class="topnav">
+  <a class="active" href="#home">Início</a>
+  <a href="#news">Currículo</a>
+  <a href="#contact">Matérias</a>
+  <a href="#about">Sobre</a>
+</div>
+
+<p id='sobre'>vovovoodsofosdfjiisdj</p>
+    </>
+  )
+}
+
+export default App
