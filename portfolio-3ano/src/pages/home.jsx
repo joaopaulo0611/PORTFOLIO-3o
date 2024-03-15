4877d0739c1ab6f14c77d85e5f04cea1d2e1d5fe
JOÃO PAULO ARAUJO CAPPELETTI <joao_cappeletti@estudante.sesisenai.org.br>
2024-03-15 10:02:42 -0300

    salvo

--- portfolio-3ano/src/pages/home.jsx
+++ portfolio-3ano/src/pages/home.jsx
@@ -9,10 +9,12 @@ function Home(){
       <Link to={'/curriculo'}><a>Currículo</a></Link>
       <Link to={'/materias'}><a>Matérias</a></Link>
     </div>
-    <div>
+    <div id="texPort">
         <h1>PORTFÓLIO </h1>
-        <h1>3° ANO</h1>
+        <h2>3° ANO</h2>
     </div>
+    <p id="antPorts"><a href="https://docs.google.com/presentation/d/1yYTOQ4iysLcwYueJDF6ozdh9WfViBUeNvfymKco-WAQ/edit?usp=sharing">PORTFÓLIO 1°</a> 
+    /<a href="https://www.canva.com/design/DAFcz1OMxSM/KTE_Cy7XJAk1O6UI8azihg/edit?utm_content=DAFcz1OMxSM&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">PORTFÓLIO 2°</a></p>
         </>
     )
 }
